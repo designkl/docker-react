c879210d1d36daba47c8673bbbed25298da11624
designkl <design@kevinlien.com>
2020-10-30 09:19:23 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          By there 34!
         </p>
         <a
           className="App-link"
